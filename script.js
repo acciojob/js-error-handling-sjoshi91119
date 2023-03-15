d6c661860dfa1254c60775b852d5e4047b73eec1
Shubham Joshi <115713904+sjoshi91119@users.noreply.github.com>
2023-03-15 17:01:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -36,14 +36,4 @@ function evalString(expression) {
   return eval(expression);
 }
 
-try {
-  console.log(evalString('2 + 3 * 4')); // Output: 14
-  console.log(evalString('2 - 3 *')); // Throws SyntaxError: Expression should not end with invalid operator
-  console.log(evalString('+ 2 + 3 * 4')); // Throws SyntaxError: Expression should not start with invalid operator
-  console.log(evalString('2 + 3 / 0')); // Throws TypeError: Division by zero
-  console.log(evalString('2 + 3 * 4.5')); // Output: 14.5
-  console.log(evalString('2 + 3 * -4')); // Output: -10
-  console.log(evalString('2++3')); // Throws InvalidExprError: Expression should not have an invalid combination of expression
-} catch (error) {
-  console.error(error.message);
-}
+
